ccb2f1ecbfdeec69de4a222f105b32367ed7c53a
Bane Bozanic <bozanicbane03@gmail.com>
2024-05-21 21:54:34 +0200

    Updatovan pRoizvodDto i ispravljeni bugovi za sesije

--- web-shop/src/main/java/com/webshop/controller/ProizvodRestController.java
+++ web-shop/src/main/java/com/webshop/controller/ProizvodRestController.java
@@ -8,6 +8,7 @@ import com.webshop.model.Kategorija;
 
 import com.webshop.model.Korisnik;
 import com.webshop.model.Proizvod;
+import com.webshop.service.KorisnikService;
 import com.webshop.service.ProizvodService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,11 +29,20 @@ public class ProizvodRestController {
 
     @Autowired
     private ProizvodService proizvodService;
-//treba pogledati da li tako ili ne
+    @Autowired
+    private KorisnikService korisnikService;
+
+    //treba pogledati da li tako ili ne
     @GetMapping
     public ResponseEntity<Page<ProizvodDto>> getProizvodi(@RequestParam int strana, HttpSession sesija) {
         Korisnik korisnik = (Korisnik) sesija.getAttribute("korisnik");
-        if(korisnik.getUloga() != Korisnik.TipKorisnika.Administrator){
+        if(korisnik == null) {
+            Page<ProizvodDto> strProizvoda = proizvodService.getProizvodi(strana, 10);
+            if (strProizvoda.isEmpty()) {
+                return new ResponseEntity("Nema proizvoda!", HttpStatus.NOT_FOUND);
+            }
+            return ResponseEntity.ok(strProizvoda);
+        } else if(korisnik.getUloga() != Korisnik.TipKorisnika.Administrator){
             Page<ProizvodDto> strProizvoda = proizvodService.getProizvodi(strana, 10);
             if (strProizvoda.isEmpty()) {
                 return new ResponseEntity("Nema proizvoda!", HttpStatus.NOT_FOUND);
@@ -46,7 +56,14 @@ public class ProizvodRestController {
     @GetMapping("/{id}")
     public ResponseEntity<ProizvodDto> getProizvod(@PathVariable("id") Long id, HttpSession sesija) {
         Korisnik korisnik = (Korisnik) sesija.getAttribute("korisnik");
-        if(korisnik.getUloga() != Korisnik.TipKorisnika.Administrator){
+        if(korisnik == null){
+            Optional<Proizvod> proizvod = proizvodService.getProizvod(id);
+            if(proizvod.isPresent()){
+                ProizvodDto prozivodDto = new ProizvodDto(id ,proizvod.get().getNaziv(), proizvod.get().getOpis(), proizvod.get().getKategorija(), proizvod.get().getCena(), proizvod.get().getSlika(), proizvod.get().getTipProdaje());
+                return ResponseEntity.status(HttpStatus.OK).body(prozivodDto);
+            }
+            else return new ResponseEntity("Takav proizvod ne postoji!", HttpStatus.NOT_FOUND);
+        } else if(korisnik.getUloga() != Korisnik.TipKorisnika.Administrator){
             Optional<Proizvod> proizvod = proizvodService.getProizvod(id);
             if(proizvod.isPresent()){
                 ProizvodDto prozivodDto = new ProizvodDto(id ,proizvod.get().getNaziv(), proizvod.get().getOpis(), proizvod.get().getKategorija(), proizvod.get().getCena(), proizvod.get().getSlika(), proizvod.get().getTipProdaje());
@@ -62,7 +79,7 @@ public class ProizvodRestController {
                                                                     @RequestBody SearchDto parametriPretrage,
                                                                     HttpSession sesija) {
         Korisnik korisnik = (Korisnik) sesija.getAttribute("korisnik");
-        if(korisnik.getUloga() != Korisnik.TipKorisnika.Administrator){
+        if(korisnik == null){
             Page<ProizvodDto> strProizvoda = proizvodService.proizvodiSearchOrFilter(parametriPretrage, strana);
             if (strProizvoda == null) {
                 return new ResponseEntity("Filteri nisu dobro primenjeni", HttpStatus.BAD_REQUEST);
@@ -70,7 +87,15 @@ public class ProizvodRestController {
             if (strProizvoda.isEmpty()) {
                 return new ResponseEntity("Nema proizvoda kojji ispunjavaju te uslove!", HttpStatus.NOT_FOUND);
             }
-
+            return ResponseEntity.ok(strProizvoda);
+        } else if(korisnik.getUloga() != Korisnik.TipKorisnika.Administrator){
+            Page<ProizvodDto> strProizvoda = proizvodService.proizvodiSearchOrFilter(parametriPretrage, strana);
+            if (strProizvoda == null) {
+                return new ResponseEntity("Filteri nisu dobro primenjeni", HttpStatus.BAD_REQUEST);
+            }
+            if (strProizvoda.isEmpty()) {
+                return new ResponseEntity("Nema proizvoda kojji ispunjavaju te uslove!", HttpStatus.NOT_FOUND);
+            }
             return ResponseEntity.ok(strProizvoda);
         }
         return new ResponseEntity("Zabranjen pristup", HttpStatus.FORBIDDEN);
@@ -79,12 +104,27 @@ public class ProizvodRestController {
     @PostMapping("/add")
     public ResponseEntity<Proizvod> postaviProizvodNaProdaju(@RequestBody ProizvodDto proizvodDto, HttpSession sesija) {
         Korisnik korisnik = (Korisnik) sesija.getAttribute("korisnik");
+        if (korisnik == null) return new ResponseEntity("Zabranjen pristup", HttpStatus.FORBIDDEN);
         if(korisnik.getUloga() == Korisnik.TipKorisnika.Prodavac){
             Proizvod proizvod = proizvodService.postaviProizvodNaProdaju(proizvodDto);
             return ResponseEntity.ok(proizvod);
         }
         return new ResponseEntity("Zabranjen pristup", HttpStatus.FORBIDDEN);
-
     }
+
+    /*@PostMapping("/api/purchase")
+    public ResponseEntity kupovina(@RequestBody ProizvodDto proizvodDto, HttpSession sesija){
+        if(sesija.getAttribute("korisnik") == null) return new ResponseEntity("Zabranjen pristup", HttpStatus.FORBIDDEN);
+        Korisnik korisnik = (Korisnik) sesija.getAttribute("korisnik");
+
+        Optional<Proizvod> proizvod = proizvodService.getProizvod(proizvodDto.getId());
+
+        if(proizvod.isPresent() && proizvod.get().isProdat() == false){
+            if(proizvod.get().getTipProdaje() == Proizvod.tipprodaje.fiksnaCena){
+
+            }
+        } else return new ResponseEntity("Prozivod je prodat ili ne postoji!", HttpStatus.NOT_FOUND);
+
+    }*/
 
 }
--- web-shop/src/main/java/com/webshop/dto/ProizvodDto.java
+++ web-shop/src/main/java/com/webshop/dto/ProizvodDto.java
@@ -58,6 +58,8 @@ public class ProizvodDto {
         this.ProdavacId = ProdavacId;
     }
 
+    public Long getId() { return id; }
+
     public String getNaziv() {
         return naziv;
     }
@@ -81,6 +83,8 @@ public class ProizvodDto {
     public Proizvod.tipprodaje getTipProdaje() {
         return tipProdaje;
     }
+
+    public void setId(Long id) { this.id = id; }
 
     public void setNaziv(String naziv) {
         this.naziv = naziv;
