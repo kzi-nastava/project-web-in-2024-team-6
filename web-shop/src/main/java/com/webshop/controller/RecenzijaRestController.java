c08c6a64ffd2ddc947a1022a375c9e54f34f8302
Kosta Gacina <psnkoleg@gmail.com>
2024-05-22 20:53:03 +0200

    admin auth bug fix

--- web-shop/src/main/java/com/webshop/controller/RecenzijaRestController.java
+++ web-shop/src/main/java/com/webshop/controller/RecenzijaRestController.java
@@ -1,9 +1,12 @@
 package com.webshop.controller;
 
 import com.webshop.dto.RecenzijaDto;
+import com.webshop.model.Korisnik;
 import com.webshop.model.Recenzija;
 import com.webshop.service.RecenzijaService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,21 +21,48 @@ public class RecenzijaRestController {
     private RecenzijaService recenzijaService;
 
     @GetMapping
-    public ResponseEntity<List<Recenzija>> vratiSveRecenzije() {
-        List<Recenzija> reviews = recenzijaService.vratiSveRecenzije();
-        return ResponseEntity.ok(reviews);
+    public ResponseEntity<List<Recenzija>> vratiSveRecenzije(HttpSession session) {
+        if (session.getAttribute("korisnik") == null) {
+            return new ResponseEntity("Zabranjen pristup", HttpStatus.UNAUTHORIZED);
+        }
+        Korisnik korisnik = (Korisnik) session.getAttribute("korisnik");
+
+        if(korisnik.getUloga() == Korisnik.TipKorisnika.Administrator) {
+            List<Recenzija> reviews = recenzijaService.vratiSveRecenzije();
+            return ResponseEntity.ok(reviews);
+        }
+
+        return new ResponseEntity("Zabranjen pristup", HttpStatus.FORBIDDEN);
     }
 
     @PutMapping("/{reviewId}")
-    public ResponseEntity<String> izmeniRecenziju(@PathVariable Long reviewId, @RequestBody RecenzijaDto recenzijaDto) {
-        recenzijaService.izmeniRecenziju(reviewId, recenzijaDto);
-        return ResponseEntity.ok("Recenzija uspešno izmenjena.");
+    public ResponseEntity<String> izmeniRecenziju(@PathVariable Long reviewId, @RequestBody RecenzijaDto recenzijaDto, HttpSession session) {
+        if (session.getAttribute("korisnik") == null) {
+            return new ResponseEntity("Zabranjen pristup", HttpStatus.UNAUTHORIZED);
+        }
+        Korisnik korisnik = (Korisnik) session.getAttribute("korisnik");
+
+        if(korisnik.getUloga() == Korisnik.TipKorisnika.Administrator) {
+            recenzijaService.izmeniRecenziju(reviewId, recenzijaDto);
+            return ResponseEntity.ok("Recenzija uspešno izmenjena.");
+        }
+        return new ResponseEntity("Zabranjen pristup", HttpStatus.FORBIDDEN);
+
     }
 
     @DeleteMapping("/{reviewId}")
-    public ResponseEntity<String> izbrisiRecenziju(@PathVariable Long reviewId) {
-        recenzijaService.izbrisiRecenziju(reviewId);
-        return ResponseEntity.ok("Recenzija uspesno izbrisana.");
+    public ResponseEntity<String> izbrisiRecenziju(@PathVariable Long reviewId, HttpSession session) {
+        if (session.getAttribute("korisnik") == null) {
+            return new ResponseEntity("Zabranjen pristup", HttpStatus.UNAUTHORIZED);
+        }
+        Korisnik korisnik = (Korisnik) session.getAttribute("korisnik");
+
+        if(korisnik.getUloga() == Korisnik.TipKorisnika.Administrator) {
+            recenzijaService.izbrisiRecenziju(reviewId);
+            return ResponseEntity.ok("Recenzija uspesno izbrisana.");
+        }
+        return new ResponseEntity("Zabranjen pristup", HttpStatus.FORBIDDEN);
+
     }
 }
 
