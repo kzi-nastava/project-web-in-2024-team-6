7b262f006ce289a23858b1d4fb35cb260f28bf7e
Bane Bozanic <bozanicbane03@gmail.com>
2024-06-23 09:06:51 +0200

    dodavanje proizvoda na prvu stranu

---
+++ front/frontend/src/components/ProductInList.vue
@@ -0,0 +1,29 @@
+<template>
+   <div clss="product">
+      <img src={{ product.slika }} alt="slikaProizvoda">
+      <h3>{{ product.naziv }}</h3>
+      <p>{{ product.opis }}</p>
+      <p>{{ product.cena }} RSD</p>
+   </div>
+      
+</template>
+
+<script>
+export default{
+   name: 'ProductInList',
+   props: {
+      product: {
+         type: Object,
+         required: true
+      }
+   }
+};
+</script>
+
+<style>
+.product{
+   border: 1px solid #ccc;
+   padding: 16px;
+   margin: 8px;
+}
+</style>
